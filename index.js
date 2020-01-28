a87f9b3dbb5c26e87ae7e1a6e28c63c3422b425a
Zachary Olpin <info@zacharyolp.in>
2020-01-28 10:13:04 -0700

    add helmet middleware

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const path = require("path");
+const helmet = require('helmet')
 
 // App variables
 const app = express();
 const port = process.env.PORT || "8000";
 
 // Config / Middlewares
+app.use(helmet())
 app.use(express.static(path.join(__dirname, "public"))); // Serve static assets/resources
 
 // Route definitions
