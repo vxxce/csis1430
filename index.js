1628778170b3e7c2c61ff200c9021e1759e5f8be
Zachary Olpin <info@zacharyolp.in>
2020-02-01 04:59:36 -0700

    use helmet and compression middlewares

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const path = require("path");
-const helmet = require('helmet')
+const helmet = require('helmet');
+var compression = require('compression');
 
 // App variables
 const app = express();
 const port = process.env.PORT || "8000";
 
 // Config / Middlewares
+app.use(compression()); //Compress all routes
 app.use(helmet())
 app.use(express.static(path.join(__dirname, "public"))); // Serve static assets/resources
 
