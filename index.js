40bd540f5c10b9a9474618dba0b1f587e50159e7
Zachary Olpin <info@zacharyolp.in>
2020-02-03 17:14:01 -0700

    static

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
 const express = require("express");
 const path = require("path");
-const helmet = require('helmet');
-var compression = require('compression');
 
 // App variables
 const app = express();
 const port = process.env.PORT || "8000";
 
 // Config / Middlewares
-app.use(compression()); //Compress all routes
-app.use(helmet())
 app.use(express.static(path.join(__dirname, "public"))); // Serve static assets/resources
 
 // Route definitions
 app.get("/", (_req, res) => {
-  res.render("index.html");
+  res.sendFile(path.join(__dirname, 'public', "index.html"));
 });
 
-app.get("/test", (_req, res) => {
-  res.render("test.html");
+app.get("/tables", (_req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'tables.html'))
 });
 
 // Server activation
