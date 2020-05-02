1b828f3e604dc671b1f4948a696ed0b903e05eac
Zachary Olpin <info@zacharyolp.in>
2020-05-02 12:42:22 -0600

    max attempts, styling

--- public/guess/js/guess.js
+++ public/guess/js/guess.js
@@ -1,30 +1,34 @@
+// ::::::::: ELEMENT CONSTANTS :::::::::::
+
 const guessInput = document.querySelector('#guess')
 const submit = document.querySelector('#submit')
 const message = document.querySelector('h1')
 const again = document.querySelector('#again')
 
 
+// ::::::::::::::: INIT :::::::::::::::::::
+
 let answer = Math.ceil(Math.random() * 99)
 let guess = guessInput.value
 let attempts = 0
 
-const newGame = () => {
-
-  // ::::::::: ELEMENT CONSTANTS :::::::::::
-
-}
 // :::::::::: GAME FUNCTIONS :::::::::::::
 
 // Checks if a guess is higher, equivalent to, or lower than answer 
 const checkGuess = (guess, answer) => (guess - answer) / Math.abs(guess - answer) ? (guess - answer) / Math.abs(guess - answer) : 0
 
-// const loadImage = async (src) => {
+// TODO: loadImage = async (src) => { }
 
-// }
+// Update view based on  guess
 
-// Update DOM to reflect guess
 const updateView = (guess, difference) => {
-  guessInput.focus()
+  guessInput.value = ""
+  attempts += 1
+  if (attempts == 8) {
+    message.textContent = "You lose."
+    again.classList.toggle('hidden')
+    return
+  }
   switch(difference){
     case -1:
       message.textContent = `${guess.toString()} is too low, guess again!`
@@ -37,16 +41,19 @@ const updateView = (guess, difference) => {
       message.textContent = `${guess.toString()} is too high, guess again!`
       break
     default:
+      again.classList.toggle('hidden')
+      message.textContent = "Guess a number between 1 and 99!"
       break
     }
 }
 
+// Handler for guess input. Checks if enter key and prevents > 2 digits
 const checkForSubmit = event => {
-      // Ignore key input beyond two characters, unless key is Enter, Backspace, or Tab
       if (event.keyCode == 13) {
         event.returnValue = true
         event.preventDefault()
         newGuess()
+      // Allows third keystroke if it's to delete or tab focus
       } else if (event.target.value.length >= 2 && event.keyCode != 8 && event.keyCode != 9) {
         event.returnValue = false
     } else {
@@ -54,11 +61,11 @@ const checkForSubmit = event => {
     }
 }
 
+// Submit guess
 const newGuess = () => {
   let guess = Number(guessInput.value)
   let diff = checkGuess(guess, answer)
   updateView(guess, diff)
-  attempts += 1
 }
 
 // ::::::::: EVENT LISTENERS ::::::::::::::
@@ -69,10 +76,10 @@ guessInput.addEventListener('focus', () => guessInput.value = "")
 // Prevent form submission on "Enter" keypress and instead updateView()
 guessInput.addEventListener('keydown', e => checkForSubmit(e))
 
-// Imitate submit event (not actually a GET or POST)
-submit.addEventListener('click', () => {
-  newGuess()
-  guessInput.focus()
+again.addEventListener('click', () => {
+  answer = Math.ceil(Math.random() * 99)
+  attempts = 0
+  updateView("", NaN)
 })
 
-window.onload = () => newGame()
+window.onload(e => updateView("", NaN))
